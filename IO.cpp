18ce14ec31c2b5ae9b4d4cf34c09935897921cda
Burak Guzel <burakguzel0218@gmail.com>
2022-06-27 17:02:57 +0200

    Modify IO.cpp

--- IO.cpp
+++ IO.cpp
@@ -11,8 +11,9 @@ int main()
     //  cout << "World!" << endl; // No space between Hello and World!
 
     //  cout << "Hello world!" << endl; // with space
-    //  cout << "Hello" << " World!" << endl; // with space
+    //  cout << "Hello" << " World!" << endl; // with space. Adding endl and \n moves to next row for the next output. 
     //  cout << "Hello" << " World!\n";   // same as above
+// Difference between \n and endl is that endl flushes the stram (akisi bosaltiyor), while \n does not!
 
 // Move to next row for each word!
     //  cout << "Hello\nOut\nThere\n" << endl; // Shows each word in next row
@@ -23,14 +24,29 @@ int main()
     //    cin >> num1;
     //    cout << "You entered: " << num1 << endl;
 
-// Enter two integers by pressing space between them
-    int num1;
-    int num2;
-    cout << "Enter the first integer: ";  // Enter two integers by pressing space between them
-    cin >> num1;
-    cout << "Enter the second first integer: ";
-    cin >> num2;
-    cout << "You entered " << num1 << " and " << num2 << endl;
- 
+// Show variables in output
+    // int num1;
+    // int num2;
+    // cout << "Enter the first integer: ";  // Enter two integers by pressing space between them
+    // cin >> num1;
+    // cout << "Enter the second integer: ";
+    // cin >> num2;
+    // cout << "You entered " << num1 << " and " << num2 << endl;
+
+// Enter two values by pressing space 
+    // int num1;
+    // int num2;
+    // cout << "Enter 2 integers separated with space: ";
+    // cin >> num1 >> num2;
+    // cout << "You entered " << num1 << " and " << num2 << endl; 
+
+// Enter a double
+    //    double num3;
+    //    cout << "Enter a double: ";
+    //    cin >> num3;
+    //    cout << "You entered: " << num3 << endl;
+
+// If user enter a double to integer variable, c++ accept it as integer and deletes decimal part!
+
     return 0;  
 }
