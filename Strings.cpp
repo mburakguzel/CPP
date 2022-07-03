7d7b475707cee5c04df747544ea37b6179373508
Burak Guzel <burakguzel0218@gmail.com>
2022-07-03 15:52:47 +0200

    Add Strings.cpp

---
+++ Strings.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+#include <cctype>    // In order to use char functions, we need to add cctype library
+using namespace std; 
+
+int main() 
+{
+    // There are two types of strings 
+        // C style strings
+        // C++ Strings
+    
+    // C style strings
+    
+
+
+
+
+    return 0;  
+}
