8a70deac8ccda7287a722b67370c82b2430719d3
Burak Güzel <burakguzel0218@gmail.com>
2023-04-18 22:15:08 +0200

    Add 2StatelessLambdaFunctions.cpp

--- LambdaFunctions/2StatelessLambdaFunctions.cpp
+++ LambdaFunctions/2StatelessLambdaFunctions.cpp
@@ -28,7 +28,6 @@
     // auto l = [] (int *x) {std::cout << *x;}; // which will change the original parameter not copy of it. * is the dereferencing operator and & is the referencing operator.
     // l(&x);
 
-// A predicate is a C++ function that takes any number of arguments and returns a boolean value.
 #include <iostream>
 #include <string>
 #include <vector>
@@ -58,61 +57,63 @@ std::ostream &operator<<(std::ostream &os, const Person &rhs) {
 void test1() {
     std::cout << "\n---Test1 --------------------------" << std::endl;
 
-    [] () {std::cout << "Hi" << std::endl; }();
+    [] () {std::cout << "Hi" << std::endl; }();         // displays Hi - if we do not add () at the end, this will not do anything!
     
-    [] (int x) { std::cout << x << std::endl;}(100);
+    [] (int x) { std::cout << x << std::endl;}(100);    // displays 100
     
-    [](int x, int y) { std::cout << x+y << std::endl; }(100,200);
+    [](int x, int y) { std::cout << x+y << std::endl; }(100,200);   // displays 300
 }
 
 // Using values and references as lambda parameters 
 void test2() {
     std::cout << "\n---Test2 --------------------------" << std::endl;
     
-    auto l1 =   [] () {std::cout << "Hi" << std::endl;};
-    l1();
+    auto l1 =   [] () {std::cout << "Hi" << std::endl;};    // assigning the lamda expression to a variable! Notice no () at the end!
+    l1();       // displays Hi
     
     int num1 {100};
     int num2 {100};
 
-    auto l2 = [](int x, int y) { std::cout << x+y << std::endl; };
-    l2(10,20);
-    l2(num1, num2);
+    auto l2 = [](int x, int y) { std::cout << x+y << std::endl; };      
+    l2(10,20);          // displays 30
+    l2(num1, num2);     // displays 200
     
+    // example with references.
     auto l3 = [](int &x, int y) {
-        std::cout << "x: " << x << " y: " << y << std::endl;
-        x = 1000;
-        y = 2000;
+        std::cout << "x: " << x << " y: " << y << std::endl;        // x: 100 y: 100
+        x = 1000;       // here we are changing the actual value of the x since it is passed by reference.
+        y = 2000;       // here we are changing the local copy of the y since it is passed by value.
     };
     
     l3(num1, num2);
-    std::cout << "num1: " << num1 << " num2: " << num2 << std::endl;
+    std::cout << "num1: " << num1 << " num2: " << num2 << std::endl;        // displays num1: 1000 num2: 100 - num2 does not change since the local copy was changed above!
 }
 
 // Using value and reference objects as lambda parameters
 void test3() {
+    // We use user defined objects instead of primitive types here!
     std::cout << "\n---Test3 --------------------------" << std::endl;
     Person stooge {"Larry", 18};
-    std::cout << stooge << std::endl;
+    std::cout << stooge << std::endl;       // displays [Person: Larry : 18]
     
-    auto l4 = [] (Person p) {
-        std::cout << p << std::endl; 
+    auto l4 = [] (Person p) {               // copied by value!
+        std::cout << p << std::endl;        
     };
-    l4(stooge);
+    l4(stooge);                             // displays [Person: Larry : 18]
     
-    auto l5 = [] (const Person &p) {
-        std::cout << p << std::endl; 
+    auto l5 = [] (const Person &p) {        // copied by reference! But since it is const, we can not change the object!
+        std::cout << p << std::endl;        
     };
-    l5(stooge);
+    l5(stooge);                             // displays [Person: Larry : 18]
     
-    auto l6 = [] (Person &p) {
+    auto l6 = [] (Person &p) {              // copied by reference and we are changing the actual value!
         p.set_name("Frank");
         p.set_age(25);
-        std::cout << p << std::endl; 
+        std::cout << p << std::endl;        
     };
-    l6(stooge);
+    l6(stooge);                             // displays [Person: Frank : 25]
     
-    std::cout << stooge << std::endl;
+    std::cout << stooge << std::endl;       // displays [Person: Frank : 25]
     
 }
 
@@ -120,14 +121,16 @@ void test3() {
 // using std::function as a parameter in C++14 and greater
 // or 
 // auto as parameter type in C++20
+// A predicate is a C++ function is that takes any number of arguments and returns a boolean value, so func(below) is a predicate function.
 void filter_vector(const std::vector<int> &vec, std::function<bool(int)> func)   {  // std::function is defined after C++14
                                                                                     // bool is the return type of func, int is the parameter type of func
+// The func function is not defined here. It is defined in test4 as a lambda function. Here it only show that filter_vector expects a function as an argument.
 // or
 // void filter_vector (const std::vector<int>  &vec, bool (*func) (int)) {          // bool is the return type of func, int is the parameter type of func, 
                                                                                     // this syntax is not used too much, it is coming from old c syntax. 
 // or                                                                          
-// void filter_vector (const std::vector<int>  &vec, auto func) {   // after C++20
-    std::cout <<"[ ";
+// void filter_vector (const std::vector<int>  &vec, auto func) {   // after C++20, above statement can be replaced with this one.
+        std::cout <<"[ ";
     for (int i: vec) {
         if (func(i)) 
             std::cout << i  << " ";
@@ -138,18 +141,23 @@ void filter_vector(const std::vector<int> &vec, std::function<bool(int)> func)  
 // passing a lambda to a function
 void test4() {
     std::cout << "\n---Test4 --------------------------" << std::endl;
-    std::vector<int> nums {10,20,30,40,50,60,70,80,90,100};
+    std::vector<int> nums {10,20,30,40,50,60,70,80,90,100};     
+
+    // The first argument of filter_vector is nums vector defined above and the second one is the lambda function. 
+    // And this is exactly how the function defined above, it expects a vector of ints. and a function object, this is explained in 1FunctionObjects.cpp.
+    filter_vector(nums, [](int x) {return x>50;});              // displays [ 60 70 80 90 100 ] - a predicate lambda expects one or more arguments and returns boolean!
+    // Above lambda function gets x as argument and returns true if x>50, as it is defined in line 132: "if (func(i))" 
+    // This only prints numbers which are greater than 50.
+
     
-    filter_vector(nums, [](int x) {return x>50;});
+    filter_vector(nums, [](int x) {return x<=30;});             // displays [ 10 20 30 ]
     
-    filter_vector(nums, [](int x) {return x<=30;});
-    
-    filter_vector(nums, [](int x) {return x>= 30 && x <=60;});
+    filter_vector(nums, [](int x) {return x>= 30 && x <=60;});  // displays [ 30 40 50 60 ]
 }
 
 // used for test5
 auto make_lambda() {
-	return [] () {std::cout << "This lambda was made using the make_lambda function!" << std::endl;};
+	return [] () {std::cout << "This lambda was made using the make_lambda function!" << std::endl;};   // make_lambda function returns a lambda
 }
 // or
 // std::function<void()> make_lambda(){
@@ -166,25 +174,24 @@ auto make_lambda() {
 void test5() {
 	std::cout << "\n---Test5 --------------------------" << std::endl;
 	
-	auto l5 = make_lambda();
+	auto l5 = make_lambda();    // since make_lmabda returns a lambda function, we can assign it to a value.
 	
-	l5();
-
+	l5();       // displays This lambda was made using the make_lambda function!
 }
 
 // using auto in the lambda parameter list
 void test6() {
     std::cout << "\n---Test6 --------------------------" << std::endl;
-    // If the lambda will be passed only one time to function define it as above, if you will pass it several times assign it to a variable so that you can call it again and again.
-    auto l6 = [](auto x, auto y) {
+    // If the lambda will be passed only one time to a function define it as above, if you will pass it several times, assign it to a variable so that you can call it again and again.
+    auto l6 = [](auto x, auto y) {      // by defining everything as auto, we are defining a generic function which can work all type of data types.
         std::cout << "x: " << x << " y: " << y << std::endl;
     };
     
-    l6(10,20);
-    l6(100.3, 200);
-    l6(12.5, 15.54);
+    l6(10,20);          // displays x: 10 y: 20
+    l6(100.3, 200);     // displays x: 100.3 y: 200
+    l6(12.5, 15.54);    // displays x: 12.5 y: 15.54
     
-    l6 (Person("Larry", 18), Person("Curly", 22));
+    l6 (Person("Larry", 18), Person("Curly", 22));  // displays x: [Person: Larry : 18] y: [Person: Curly : 22]
 }
 
 // Simple lambda examples with std::sort and std::for_each
@@ -196,22 +203,22 @@ void test7() {
         {"Moe", 30},
         {"Curly", 25}
     };
-    std::sort(begin(stooges), end(stooges), [](const Person &p1, const Person &p2) {
-        return p1.get_name() < p2.get_name();
+    std::sort(begin(stooges), end(stooges), [](const Person &p1, const Person &p2) {    // std::sort is an stl algorithm. It expects a function object.
+        return p1.get_name() < p2.get_name();       // defining the sort condition here!
     });
     
-    std::for_each(begin(stooges), end(stooges), [](const Person &p) {
-        std::cout << p << std::endl;
+    std::for_each(begin(stooges), end(stooges), [](const Person &p) {           // std::for_each is an stl algorithm. It expects a function object.
+        std::cout << p << std::endl;        // displays [Person: Curly : 25], displays [Person: Larry : 18], displays [Person: Moe : 30] 
     });
     
     std::cout << std::endl;
     
      std::sort(begin(stooges), end(stooges), [](const Person &p1, const Person &p2) {
-        return p1.get_age()< p2.get_age();
+        return p1.get_age()< p2.get_age();  // defining the sort condition here!
     });
     
     std::for_each(begin(stooges), end(stooges), [](const Person &p) {
-        std::cout << p << std::endl;
+        std::cout << p << std::endl;        // displays [Person: Larry : 18], displays [Person: Curly : 25], displays [Person: Moe : 30]
     });
 }
 
